68327bbbba508bac05f6888048541d63cd510eb1
jonnystalnaker <steamsynthetic@gmail.com>
2024-06-04 15:35:26 -0700

    showing million npm package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import MillionLint from 'million/compiler';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [[MillionLint.vite()], react()],
+});
